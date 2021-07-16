61159d01ce81725f782b72560e141d06d4346cb9
MichalRub <michal.rubajczyk@pep.pl>
2021-07-16 12:58:06 +0200

    Add some logs

--- app/src/main/java/com/pep/paxserver/aidl/PaxServerRemoteService.kt
+++ app/src/main/java/com/pep/paxserver/aidl/PaxServerRemoteService.kt
@@ -4,31 +4,40 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.os.Process
+import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
 import pl.pep.peplinker.PePLinkerBinder
 
 @AndroidEntryPoint
 class PaxServerRemoteService : Service() {
 
+    companion object {
+        private const val TAG = "PaxServerRemoteService"
+    }
+
     private var linkerBinder: PePLinkerBinder? = null
 
     override fun onCreate() {
         super.onCreate()
+        Log.d(TAG, "onCreate()")
         linkerBinder = PePLinkerBinder.Factory.newBinder()
         linkerBinder?.registerObject(paxServerRemoteService)
     }
 
     override fun onBind(intent: Intent?): IBinder? {
+        Log.d(TAG, "onBind()")
         return linkerBinder
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d(TAG, "onDestroy()")
         linkerBinder?.unRegisterObject(paxServerRemoteService)
     }
 
     private val paxServerRemoteService: IPaxServerRemoteService = object : IPaxServerRemoteService {
         override fun getPid(): Int {
+            Log.d(TAG, "server getPid()")
             return Process.myPid()
         }
 
